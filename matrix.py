24df5fd6907eb9aa70ec4a47eb4bfd4469021961
kashyaptan <kashyaptan2004@gmail.com>
2025-02-01 00:34:34 -0500

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,2 +1,12 @@
-import math
-print(math.gcd(290184, 351))
+    def euclidean_gcd(x, y):
+        r = x % y
+        while r != 0:
+            q = x // y
+            r = x % y
+            print(f"{x} = {y} * {q} + {r}")
+            x = y
+            y = r
+        print(f"GCD = {x}")
+        return r
+
+    euclidean_gcd(356519, 114151)
