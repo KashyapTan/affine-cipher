314f65eb47038bea99ffd3438766cd04f3d17345
kashyaptan <kashyaptan2004@gmail.com>
2025-02-01 00:06:38 -0500

    .

--- main.py
+++ main.py
@@ -5,24 +5,44 @@ affine_cipher = {
 
 reverse_affine_cipher = {a: b for b, a in affine_cipher.items()}
 
-plaintext = "howareyou"
+plaintext = "gezxds"
 numerical_plaintext = [affine_cipher[char] for char in plaintext]
-print('Converting howareyou into corresponding numbers: ', numerical_plaintext)
+print('Converting solved into corresponding numbers: ', numerical_plaintext)
 
-def encrypt(x):
-    return (5*x + 7) % 26
+# def encrypt(x):
+#     return (5*x + 7) % 26
 
-def decrypt(x):
-    return (21*(x-7)) % 26
+# def decrypt(x):
+#     return (21*(x-7)) % 26
 
-encrypted_numbers = [encrypt(num) for num in numerical_plaintext]
-print('Encrypted numbers', encrypted_numbers)
+# encrypted_numbers = [encrypt(num) for num in numerical_plaintext]
+# print('Encrypted numbers', encrypted_numbers)
 
-encrypted_message = ''.join([reverse_affine_cipher[num] for num in encrypted_numbers])
-print('Encrypted message:', encrypted_message)
+# encrypted_message = ''.join([reverse_affine_cipher[num] for num in encrypted_numbers])
+# print('Encrypted message:', encrypted_message)
 
-decrypted_numbers = [decrypt(num) for num in encrypted_numbers]
-print('Decrypted numbers', decrypted_numbers)
+# decrypted_numbers = [decrypt(num) for num in encrypted_numbers]
+# print('Decrypted numbers', decrypted_numbers)
 
-decrypted_message = ''.join([reverse_affine_cipher[num] for num in decrypted_numbers])
-print('Decrypted message:', decrypted_message)
+# decrypted_message = ''.join([reverse_affine_cipher[num] for num in decrypted_numbers])
+# print('Decrypted message:', decrypted_message)
+
+# def solve_affine_keys():
+#     # From equation 2, k2 = 14
+#     k2 = 14
+    
+#     # Find k1 where 7k1 ≡ 25 (mod 26)
+#     for k1 in range(26):
+#         if (7 * k1) % 26 == 25:
+#             return k1, k2
+            
+#     return None, None
+
+# k1, k2 = solve_affine_keys()
+# print(f"k1 = {k1}")
+# print(f"k2 = {k2}")
+
+# # Verify the solution
+# print(f"Verification:")
+# print(f"7k1 + k2 mod 26 = {(7*k1 + k2) % 26}")  # Should be 13
+# print(f"0k1 + k2 mod 26 = {(0*k1 + k2) % 26}")  # Should be 14
